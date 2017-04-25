5b0bd9d50f4ebf2e5a7747bcc15ec54e58897cc7
kadekbagus <kadek@dominopos.com>
2017-04-25 15:24:47 +0800

    initial files

---
+++ index.php
@@ -0,0 +1,8 @@
+
+<?php
+    //echo get_bloginfo('stylesheet_url');
+    //print_r(get_available_languages());
+    //echo get_locale();
+    $user = get_user_by( 'id', 2);
+    var_dump($user);
+?>
